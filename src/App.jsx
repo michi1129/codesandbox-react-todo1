5d140579fbb7ac533a7995e34f9c507d928864a8
ro <ro.michi1129@gmail.com>
2021-09-20 13:51:35 +0900

    53.追加ボタン

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいいい"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["うううううう"]);
+
+  const todoTextOnChange = (event) => setTodoText(event.target.value);
+  const addOnClick = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={todoTextOnChange}
+        />
+        <button onClick={addOnClick}>追加</button>
       </div>
       <div className="incomplete-area">
         <div>
