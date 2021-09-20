e2a51f8f269d562007a1481ceee6bc54ea1e789a
ro <ro.michi1129@gmail.com>
2021-09-20 13:32:40 +0900

    50.構造のベースを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <div>
+          <p>未完了のTODO</p>
+          <ul>
+            <li>
+              <div>
+                <span>ああああ</span>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+            <li>
+              <div>
+                <span>いいいいい</span>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>完了したTODO</p>
+          <ul>
+            <li>
+              <div>
+                <span>うううう</span>
+                <button>戻す</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
